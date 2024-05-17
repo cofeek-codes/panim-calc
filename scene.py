cad84ee48127ac6c96cd00fa9bede7856e730679
cofeek-codes <kormyshev11@mail.ru>
2024-05-17 14:38:20 +0500

    `calc_buttons` in progress

--- scene.py
+++ scene.py
@@ -4,6 +4,13 @@ from manim import *
 CALCULATOR_WIDTH = 5
 CALCULATOR_HEIGHT = 5
 
+calc_buttons_rects = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+
+]
+
 
 class Calculator(Scene):
     def construct(self):
@@ -15,7 +22,15 @@ class Calculator(Scene):
                              [CALCULATOR_WIDTH/2, (CALCULATOR_HEIGHT*0.25), 0])
         calc_out_line.set_color(ORANGE)
         # calculator buttons
-        calc_buttons = Text(str(1), font='sans-serif', color=ORANGE)
+        for i in range(1, 3):
+            for j in range(1, 3):
+                r = RoundedRectangle(width=1, height=1, corner_radius=0.1)
+                r.move_to([
+                    calc_out_line.points[0][0],  # line start x
+                    calc_out_line.points[0][1],  # line start y
+                    0
+                ])
+                self.play(Create(r))
+
         self.play(Create(calc_rect))
         self.play(Create(calc_out_line))
-        self.play(Write(calc_buttons))
